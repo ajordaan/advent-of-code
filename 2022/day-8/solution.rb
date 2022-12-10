f72e21fb5d1d691ba12b791d176dc6d6f8bf1f56
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-10 18:36:17 +0200

    Add part 2 and refactor

--- 2022/day-8/solution.rb
+++ 2022/day-8/solution.rb
@@ -1,6 +1,9 @@
+def edge_tree?(row, col)
+  row == 0 || row == ROW_SIZE - 1 || col == 0 || col == COL_SIZE - 1
+end
+
 def tree_visible?(grid, row, col)
-  edge_tree = row == 0 || row == ROW_SIZE - 1 || col == 0 || col == COL_SIZE - 1
-  return true if edge_tree
+  return true if edge_tree? row, col
   
   visible_directions = DIRECTIONS.map { |direction| tree_visible_in_direction? grid, row, col, direction }
   visible_directions.any? 
@@ -8,40 +11,95 @@ end
 
 def tree_visible_in_direction?(grid, row, col, direction)
   tree_height = grid[row][col]
-  case direction
-  when :up
-    index = row - 1
-    trees = []
-    while index >= 0
-      trees << grid[index][col]
-      index -= 1
+  trees = case direction
+    when :up
+      index = row - 1
+      trees = []
+      while index >= 0
+        trees << grid[index][col]
+        index -= 1
+      end
+      trees
+    when :down
+      index = row + 1
+      trees = []
+      while index < ROW_SIZE
+        trees << grid[index][col]
+        index += 1
+      end
+      trees
+    when :left
+      index = col - 1
+      trees = []
+      while index >= 0
+        trees << grid[row][index]
+        index -= 1
+      end
+      trees
+    when :right
+      index = col + 1
+      trees = []
+      while index < COL_SIZE
+        trees << grid[row][index]
+        index += 1
+      end
+      trees
     end
-    return trees.all? { |tree| tree < tree_height }
-  when :down
-    index = row + 1
-    trees = []
-    while index < ROW_SIZE
-      trees << grid[index][col]
-      index += 1
-    end
-    return trees.all? { |tree| tree < tree_height }
-  when :left
-    index = col - 1
-    trees = []
-    while index >= 0
-      trees << grid[row][index]
-      index -= 1
-    end
-    return trees.all? { |tree| tree < tree_height }
-  when :right
-    index = col + 1
-    trees = []
-    while index < COL_SIZE
-      trees << grid[row][index]
-      index += 1
-    end
-    return trees.all? { |tree| tree < tree_height }
+
+  trees.all? { |tree| tree < tree_height }
+end
+
+def tree_score(grid, row, col)
+  return 0 if edge_tree? row, col
+
+  view_distance_scores = DIRECTIONS.map {|direction| tree_view_distance_in_direction grid, row, col, direction}
+  view_distance_scores.reduce(:*)
+end
+
+def tree_view_distance_in_direction(grid, row, col, direction)
+  tree_height = grid[row][col]
+  trees = case direction
+    when :up
+      index = row - 1
+      trees = []
+      while index >= 0
+        trees << grid[index][col]
+        index -= 1
+      end
+     trees
+    when :down
+      index = row + 1
+      trees = []
+      while index < ROW_SIZE
+        trees << grid[index][col]
+        index += 1
+      end
+      trees
+    when :left
+      index = col - 1
+      trees = []
+      while index >= 0
+        trees << grid[row][index]
+        index -= 1
+      end
+      trees
+    when :right
+      index = col + 1
+      trees = []
+      while index < COL_SIZE
+        trees << grid[row][index]
+        index += 1
+      end
+      trees
   end
+
+  score = 0
+  for tree in trees
+    score += 1
+    break if tree >= tree_height
+  end
+
+  score
 end
 
 tree_grid = File.readlines('input.txt').map {|line| line.strip.chars}
@@ -49,17 +107,20 @@ tree_grid = File.readlines('input.txt').map {|line| line.strip.chars}
 ROW_SIZE = tree_grid.size
 COL_SIZE = tree_grid.first.size
 
-DIRECTIONS = [:up, :down, :left, :right]
+DIRECTIONS = [:up, :left, :down, :right]
 
 visible_tree_count = 0
+scenic_scores = []
 
 tree_grid.each_with_index do |trees, row|
   trees.each_with_index do |tree, col|
-    if tree_visible? tree_grid, row, col
-      visible_tree_count += 1
-    end
+    visible_tree_count += 1 if tree_visible? tree_grid, row, col
+    scenic_scores << tree_score(tree_grid, row, col)
   end
 end
 
 puts "Trees Visible: "
 p visible_tree_count
+
+puts "Highest Scenic Score: "
+p scenic_scores.max
