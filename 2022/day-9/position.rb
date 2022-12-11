2acc765bbba54da79584d24a3cf8a1f624e01312
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-11 20:26:58 +0200

    Move update position method to class

--- 2022/day-9/position.rb
+++ 2022/day-9/position.rb
@@ -4,20 +4,53 @@ class Position
 
   DIRECTIONS = { U: { coordinate: :y, operator: :+ }, D: { coordinate: :y, operator: :- }, L: { coordinate: :x, operator: :- }, R: { coordinate: :x, operator: :+ } }
 
-  def initialize(x: 0,y: 0, name:, follows: nil)
+  def initialize(x: 0,y: 0, name:, follower: nil )
     @x = x
     @y = y
     @name = name
-    @follows = follows
+    @follower = follower
 
     @moves = Set.new
   end
 
   attr_reader :x, :y, :name, :moves
+  attr_accessor :follower
 
   def move(direction)
     move_direction = DIRECTIONS[direction.to_sym]
     update_coordinate move_direction[:coordinate], move_direction[:operator]
+
+    follower.follow self if follower
+  end
+
+  def follow(leader)
+    if name == 2
+      puts '2 following'
+
+      puts leader.to_s
+      puts to_s
+    end
+    return if next_to? leader
+  
+    puts 'moving 2' if name == 2
+    if in_same_row? leader
+      puts 'in same row as 1' if name == 2
+      move(below?(leader) ? :U : :D)
+    elsif in_same_column? leader
+      puts 'in same column as 1' if name == 2
+      move(left_of?(leader) ? :R : :L)
+    else
+        puts 'in different row' if name == 2
+      if below? leader
+        puts '2 is below' if name == 2
+        move :U
+      else
+        move :D
+      end
+        move(left_of?(leader) ? :R : :L)
+    end
+  
+    count_move "(#{x},#{y})"
   end
 
   def count_move(coords)
@@ -36,7 +69,7 @@ class Position
     y > other.y
   end
 
-  def below?
+  def below?(other)
     y < other.y
   end
 
@@ -57,7 +90,7 @@ class Position
   end
 
   def to_s
-    "#{name} (x: #{@x}, y: #{@y})"
+    "#{name} (x: #{@x}, y: #{@y}) Follower: #{@follower&.name}"
   end
 
   private 
--- 2022/day-9/solution.rb
+++ 2022/day-9/solution.rb
@@ -1,13 +1,59 @@
 require_relative 'position'
 
+START_X = 0
+START_Y = 0
+NUMBER_OF_KNOTS = 9
 def part_one
-  head = Position.new x: 100, y: 100, name: 'H'
-  tail = Position.new x: 100, y: 100, name: 'T', follows: head
+  tail = Position.new x: START_X, y: START_Y, name: 'T'
+  head = Position.new x: START_X, y: START_Y, name: 'H', follower: tail
 
-  tail.count_move "(100,100)"
+  tail.count_move "(#{START_X},#{START_Y})"
+
+  knots = [head, tail]
 
   puts "== INITIAL STATE ==\n"
-  print_grid [head, tail]
+  print_grid knots
+  puts ' '
+  File.foreach('input.txt') do |l|
+
+    direction, distance = l.split ' '
+    distance = distance.to_i
+    puts "== #{direction} #{distance} =="
+
+    distance.times do
+      head.move direction
+
+      puts ' '
+      print_grid knots
+      puts ' '
+    end
+  end
+
+  puts "Moves: "
+  puts tail.moves.size
+end
+
+def part_two
+  head = Position.new x: START_X, y: START_Y, name: 'H', follower: nil
+  prev_knot = nil
+  knots = []
+  NUMBER_OF_KNOTS.downto(1) do |n|
+   k = Position.new x: START_X, y: START_Y, name: n, follower: prev_knot
+   knots << k
+   prev_knot = k
+  end
+
+  head.follower = knots.last
+  knots << head
+  knots.reverse!
+
+  knots.each {|k| k.count_move("#{START_X},#{START_Y}")}
+
+  puts knots
+
+
+  puts "== INITIAL STATE ==\n"
+  print_grid knots
   puts ' '
   File.foreach('example_input.txt') do |l|
 
@@ -17,42 +63,19 @@ def part_one
 
     distance.times do
       head.move direction
-      update_tail head, tail
-
       puts ' '
-      print_grid [head, tail]
+      print_grid knots
       puts ' '
     end
   end
 
   puts "Moves: "
-  puts tail.moves.size
+  puts knots.last.moves.size
 end
 
-
-def update_tail(head, tail)
-  return if head.next_to? tail
-
-  if head.in_same_row? tail
-    tail.move(head.above?(tail) ? :U : :D)
-  elsif head.in_same_column? tail
-    tail.move(head.right_of?(tail) ? :R : :L)
-  else
-    if head.above? tail
-      tail.move :U
-    else
-      tail.move :D
-    end
-      tail.move(head.right_of?(tail) ? :R : :L)
-  end
-
-  tail.count_move "(#{tail.x},#{tail.y})"
-end
-
-
-def print_grid(knots, start = 100)
+def print_grid(knots, start = START_X)
   (start + 4).downto(start) do |r|
-    start.upto(start + 6) do |c|
+    start.upto(start + 5) do |c|
       found_knot = knots.find { |k| k.x == c && k.y == r }
 
       if found_knot
