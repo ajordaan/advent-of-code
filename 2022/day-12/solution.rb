3a9395d57a6dd88a3c135649565ccb2c32716ada
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-13 22:49:20 +0200

    add part one

--- 2022/day-12/solution.rb
+++ 2022/day-12/solution.rb
@@ -1,147 +1,115 @@
 require_relative 'node'
 require_relative '../utils'
+INFINITY = 9999999
 
-def valid_move?(prev, n)
+def valid_move?(prev, n, grid_values)
+
+  unless (grid_values[n].ord - grid_values[prev].ord) <= 1 
+    return false
+  end
+
   moves = 0
-  if prev.split('')[0].to_i > n.split('')[0].to_i
+  if prev.split(',')[0].to_i > n.split(',')[0].to_i
     moves += 1
   end
-  if prev.split('')[0].to_i < n.split('')[0].to_i
+  if prev.split(',')[0].to_i < n.split(',')[0].to_i
     moves += 1
   end
-  if prev.split('')[1].to_i < n.split('')[1].to_i
+  if prev.split(',')[1].to_i < n.split(',')[1].to_i
     moves += 1
   end
-  if prev.split('')[1].to_i > n.split('')[1].to_i
+  if prev.split(',')[1].to_i > n.split(',')[1].to_i
     moves += 1
   end
 
   moves < 2
 end
 
-grid = File.readlines('example_input.txt').map {|line| line.strip.chars}
+grid = File.readlines('input.txt').map {|line| line.strip.chars}
 p grid
 nodes = []
 id = 1
 
 grid_values = {}
-node_sibling_hash = {}
+node_siblings = {}
+
+start = nil
+dest = nil
 grid.each_with_index do |x, row|
   max_row_size = grid.size
   max_col_size = x.size
 
   x.each_with_index do |_, col|
     print grid[row][col]
+    node_pos = "#{row},#{col}"
 
-    grid_values["#{row}#{col}"] = grid[row][col]
-    node_sibling_hash["#{row}#{col}"] ||= []
-    id += 1
-
-    p nodes
-    n = nodes.find {|no| no.row == row && no.col == col } || Node.new( value: grid[row][col], row: row, col: col, id: id)
-    puts "Current Node ID: #{n.id}"
-    puts "#{row}, #{col}"
+    if grid[row][col] == 'S'
+      start = node_pos
+    end
+    if grid[row][col] == 'E'
+      dest = node_pos
+    end
+ 
+    nodes << node_pos
+    grid_values[node_pos] = grid[row][col]
+    node_siblings[node_pos] ||= []
 
     if (row + 1) < max_row_size
-      sib = Node.new(value: grid.at(row + 1).at(col), row: row + 1, col: col, id: id)
-      
-      id += 1 
-      existing_sib = nodes.find {|no| no.row == (row + 1) && no.col == (col)  }
-      puts "found existing" if existing_sib
-      n.add_sibling(row + 1, col)
-
+      node_siblings[node_pos] << "#{row + 1},#{col}"
     end
     if row != 0
-      sib = Node.new(value: grid.at(row - 1).at(col), row: row - 1, col: col, id: id)
-      id += 1
-      existing_sib = nodes.find {|no| no.row == (row-1) && no.col == (col)  }
-      puts "found existing" if existing_sib
-      n.add_sibling(row - 1, col)
+      node_siblings[node_pos] << "#{row - 1},#{col}"
     end
     if (col + 1) < max_col_size
-      sib = Node.new(value: grid.at(row).at(col + 1), row: row, col: col + 1, id: id)
-      id += 1
-      existing_sib = nodes.find {|no| no.col == (col+1) && no.row == row  }
-      puts "found existing" if existing_sib
-      n.add_sibling(row, col + 1)
+      node_siblings[node_pos] << "#{row},#{col + 1}"
     end
-    
     if col != 0
-      sib = Node.new(value: grid.at(row).at(col - 1), row: row, col: col - 1, id: id)
-      existing_sib = nodes.find {|no| no == sib  }
-      puts "found existing" if existing_sib
-      n.add_sibling(row, col - 1)
+      node_siblings[node_pos] << "#{row},#{col - 1}"
     end
-
-   nodes << n
-
-   puts "Added #{n}"
-
-   id += 1
   end
   puts ''
-  puts "SS #{nodes.first.siblings.first}"
 end
-puts "SS #{nodes.first.siblings.first}"
 
-
-puts nodes
-
+puts "\nNodes"
+p nodes
+puts "\nSiblings"
+p node_siblings
+puts "\nValues"
 p grid_values
 
-grid_values["00"] = 'a'
-puts "\nBFS\n"
+grid_values[start] = 'a'
+grid_values[dest] = 'z'
 
-queue = [nodes.first.position]
-visited_nodes = []
-path = []
 
-until queue.empty?
-  n = queue.shift
-  while !path.empty? && !valid_move?(path.last, n)
-    n = queue.shift
-  end
-  
+puts "Start: #{start}"
+puts "Dest: #{dest}"
 
-  if !path.empty?
-    prev = path.last
-    if prev.split('')[0].to_i > n.split('')[0].to_i
-      puts "Moving Up"
-    end
-    if prev.split('')[0].to_i < n.split('')[0].to_i
-      puts "Moving Down"
-    end
-    if prev.split('')[1].to_i < n.split('')[1].to_i
-      puts "Moving Right"
-    end
-    if prev.split('')[1].to_i > n.split('')[1].to_i
-      puts "Moving Left"
-    end
-  else
-    puts "Start"
-  end
 
-  puts "Visiting #{grid_values[n]} (#{n})"
-  break if grid_values[n] == 'E'
- 
-  visited_nodes << n
-  path << n
-  for sibling in nodes.find { |no| no.position == n }.siblings
+visited = {}
 
-    if grid_values[sibling] == 'E'
-      break
+queue = [[start, 0]]
+found = false
+
+puts "Queue: #{queue}"
+until queue.empty? || found
+  curr = queue.shift
+
+  curr_position = curr[0]
+
+  valid_siblings = node_siblings[curr_position].filter {|sib| valid_move?(curr_position, sib, grid_values)}
+
+  valid_siblings.each do |sibling|
+    if sibling == dest
+      puts "\n\n\n FOUND \n\n\n"
+      puts (curr[1] + 1)
+
+      p queue
+      found = true
     end
-    sibling_val = grid_values[sibling]
-    sibling_val = 'z' if grid_values[sibling] == 'E'
-    unless visited_nodes.include?(sibling) || ((grid_values[n].ord - sibling_val.ord) < -1 )
-      queue << sibling
-      visited_nodes << sibling
+
+    unless visited.has_key? sibling
+      visited[sibling] = true
+      queue << [sibling, curr[1] + 1]
     end
-   
   end
 end
-
-path.each {|v| print "#{grid_values[v]} -> "}
-puts ''
-puts path.size
- 
