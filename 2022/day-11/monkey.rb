8221141c79511ce82afdf49bde7d054cdb7824ef
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-13 17:34:51 +0200

    add day 11 part 2

--- 2022/day-11/monkey.rb
+++ 2022/day-11/monkey.rb
@@ -1,18 +1,39 @@
 class Monkey
   def initialize(name:)
     @name = name
+    @inspected_items = 0
   end
 
-  attr_accessor :name, :items, :operation, :test_division_number, :true_throw_monkey, :false_throw_monkey
+  attr_accessor :name, :mod, :items, :inspected_items, :operation, :test_division_number, :true_throw_monkey, :false_throw_monkey
 
   def calculate_worry_level(item)
-    op = @operation.gsub 'old', item
+    @inspected_items += 1
+    
+    op = @operation.gsub 'old', item.to_s
     worry_level = eval op
-
-    (worry_level / 3.0).round
+    (worry_level.to_i / 3.0).to_i
   end
 
-  def throw_item_to(item, monkey)
+  def calculate_worry_level_p2(item)
+    @inspected_items += 1
+    
+    op = @operation.gsub 'old', item.to_s
+    worry_level = eval op
+    worry_level.to_i % @mod
+  end
+
+  def test(value)
+    value % test_division_number == 0
+  end
+
+  def test_p2(value)
+
+  end
+
+  def throw_item(item) 
+    monkey = test(item) ? true_throw_monkey : false_throw_monkey
+
+    # puts "item #{item} thrown to monkey #{monkey.name}"
     monkey.catch_item item 
   end
 
@@ -21,7 +42,7 @@ class Monkey
   end
 
   def to_s
-    "Name: #{name}\nItems: #{items}\nTrue throw: #{true_throw_monkey.name}\nFalse Throw: #{false_throw_monkey.name}}"
+    "Monkey #{name} inspected items #{inspected_items} times"
   end
 
 end
--- 2022/day-11/solution.rb
+++ 2022/day-11/solution.rb
@@ -1,7 +1,42 @@
 require_relative '../utils'
 require_relative 'monkey'
+
+MAX_ROUNDS = 10000
+
 def part_one
-load_monkeys 'example_input.txt'
+
+  starting = Time.now
+
+
+  monkeys = load_monkeys 'input.txt'
+
+  1.upto(MAX_ROUNDS) do |round|
+    monkeys.each do |monkey|
+      monkey.items.each do |item|
+        worry_level = monkey.calculate_worry_level_p2 item
+        monkey.throw_item worry_level
+      end
+      monkey.items = []
+    end
+    print_monkeys monkeys if round % 1000 == 0 || round == 20 || round == 1
+  end
+
+  ending = Time.now
+elapsed = ending - starting
+ 
+monkeys.sort! {|a,b| b.inspected_items <=> a.inspected_items}
+
+monkeys.each {|m| p [m.name, m.inspected_items]}
+puts "\n"
+puts (monkeys[0].inspected_items * monkeys[1].inspected_items)
+
+puts "Time Taken: #{elapsed.to_i} seconds"
+
+end
+
+def print_monkeys(monkeys)
+  monkeys.each {|m| puts m}
+  puts ''
 end
 
 def load_monkeys(filename)
@@ -9,22 +44,24 @@ def load_monkeys(filename)
   monkey = nil
   true_monkey_throws_to = {}
   false_monkey_throws_to = {}
+  mod = 1
   File.foreach(filename) do |l|
     line = l.split " "
-    p line
+
     case line[0]
     when 'Monkey'
      monkey = Monkey.new name: monkeys.size
      monkeys << monkey
     when 'Starting'
-      items = l.split(':').last.split(',').map(&:strip)
+      items = l.split(':').last.split(',').map(&:strip).map(&:to_i)
       monkey.items = items
     when 'Operation:'
       operation = l.split('=').last.strip
       monkey.operation = operation
     when 'Test:'
-      divisible = line.last
+      divisible = line.last.to_i
       monkey.test_division_number = divisible
+      mod *= divisible
     when 'If'
       if line[1] == 'true:'
         true_monkey_throws_to[monkey] = line.last.to_i
@@ -32,10 +69,9 @@ def load_monkeys(filename)
         false_monkey_throws_to[monkey] = line.last.to_i
       end
     end
-
   end
 
-  puts "throws to"
+  monkeys.each {|m| m.mod = mod}
 
   true_monkey_throws_to.each do |monkey_throwing, monkey_catching_name|
     monkey_throwing.true_throw_monkey = monkeys.find {|m| m.name == monkey_catching_name}
@@ -44,12 +80,8 @@ def load_monkeys(filename)
   false_monkey_throws_to.each do |monkey_throwing, monkey_catching_name|
     monkey_throwing.false_throw_monkey = monkeys.find {|m| m.name == monkey_catching_name}
   end
-  p monkeys
-  
-  monkeys.each do |m|
-   puts m.to_s
 
-  end
+  monkeys
 end
 
 part_one
