2a5f377c8a52157313babb72b517d64329a8bdad
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-12-13 14:37:57 +0200

    day 13 part 1, feels better but still wrong (5388)

--- 2022/day-13/solution.rb
+++ 2022/day-13/solution.rb
@@ -3,59 +3,58 @@ PacketPair = Struct.new(:left, :right, :index, :correct_order)
 
 def part_one
   packets = load_packets 'input.txt'
-
+ correct_indexes = []
   packets.each do |pair|
-    
-    larger = pair.left.size >= pair.right.size ? pair.left : pair.right
+    puts "\n== Pair #{pair.index} =="
 
-   puts "\n== Pair #{pair.index} =="
+    puts "Compare #{pair.left} vs #{pair.right}"
+    result = compare_packets(pair.left, pair.right) 
+    puts "\n#{result}\n"
 
-   puts "Compare #{pair.left} vs #{pair.right}"
-   result = larger.each_with_index.map do |_, i|
-     puts "\n==IN Map=="
-     puts "Comparing at index #{i}"
-     print "left[i] = "
-     p pair.left[i]
-     print "right[i] = "
-     p pair.right[i]
-     puts ''
-     compare_packets(pair.left[i] || [], pair.right[i] || [], pair.left, pair.right)
-   end 
+    correct_indexes << pair.index if result
+    pair.correct_order = result
+  end
 
-   pair.correct_order = result.compact.first
- end
+  puts correct_indexes
 
-answer = packets.filter {|p| p.correct_order}.reduce(0) {|sum, pair| sum += pair.index}
+  puts correct_indexes.sum
 
-puts answer
+ answer = packets.filter {|p| p.correct_order}.reduce(0) {|sum, pair| sum += pair.index}
+
+ puts answer
 end
 
-def compare_packets(left, right, left_parent = nil, right_parent = nil, index = 0)
+def compare_packets(left, right)
   puts "TOP: comparing #{left} vs #{right}"
-  puts "Left parent: #{left_parent}"
-  puts "Right parent: #{right_parent}"
+
   if left.kind_of?(Integer) && right.kind_of?(Integer)
+    puts "Comparing #{left} vs #{right}"
     puts "Left side is smaller, so inputs are in the right order" if left < right
     return true if left < right
     puts "Right side is smaller, so inputs are not in the right order" if left > right
     return false if left > right
-    puts "Numbers equal"
-    index += 1
-    puts "LEft index: #{index}"
-    puts "Right index: #{index}"
-    puts "LEft next: #{left_parent&.at(index)}"
-    puts "Right next: #{right_parent&.at(index)}"
-
-    return compare_packets(left_parent&.at(index),right_parent&.at(index), left_parent, right_parent, index) if !left_parent&.at(index).nil? && !right_parent&.at(index).nil?
-  
+   
+    return nil
   end
 
   if left.kind_of?(Array) && right.kind_of?(Array)
     puts "Left side ran out of items so items in the correct order" if left.empty? && right.any?
     return right.any? if left.empty? 
-puts "Right side ran out of items, so inputs are not in the right order" if right.empty? && left.any? 
-    return false if right.empty? && left.any?
-    return compare_packets(left.first, right.first, left_parent = left, right_parent = right)
+    puts "Right side ran out of items, so inputs are not in the right order" if right.empty? && left.any? 
+    return false if right.empty? && left.any? 
+    smaller = left.size < right.size ? left.size : right.size
+
+    i = 0
+    while i < smaller
+      res = compare_packets(left.at(i), right.at(i))
+      return res unless res.nil?
+      i += 1
+    end
+
+    return true if i == left.size && i < right.size
+    return false if i == right.size && i < left.size
+
+    return nil
   end
 
   if left.kind_of?(Array) && right.kind_of?(Integer)
@@ -102,29 +101,17 @@ def compare_packets_test
   expected_values = [0, true, true, false, true, false, true, false, false]
 
   packets.each do |pair|
-    
-     larger = pair.left.size >= pair.right.size ? pair.left : pair.right
-
     puts "\n== Pair #{pair.index} =="
 
     puts "Compare #{pair.left} vs #{pair.right}"
-    result = larger.each_with_index.map do |_, i|
-      puts "\n==IN Map=="
-      puts "Comparing at index #{i}"
-      print "left[i] = "
-      p pair.left[i]
-      print "right[i] = "
-      p pair.right[i]
-      puts ''
-      compare_packets(pair.left[i] || [], pair.right[i] || [], pair.left, pair.right)
-    end 
-
-    pair.correct_order = result.compact.first
-    if result.compact.first == expected_values[pair.index]
+    result = compare_packets(pair.left, pair.right) 
+    puts "\n#{result}\n"
+    pair.correct_order = result
+    if result == expected_values[pair.index]
       puts result
       puts "PASS".green
     else
-      puts "Expected #{expected_values[pair.index]}, got #{result.compact.first}"
+      puts "Expected #{expected_values[pair.index]}, got #{result}"
       puts "FAIL".red
     end
   end
